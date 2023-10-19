1c17a6da02c56a739491734ae77aa47e18eb2e1a
Tiffany GIBIER <tgibier@made-f0Br4s10.clusters.42paris.fr>
2023-10-19 16:30:48 +0200

    changed world.h for merge

--- inc/wonderland.h
+++ inc/wonderland.h
@@ -24,6 +24,7 @@
 # include "libft.h"
 # include "colors.h"
 
+
 # define WIDTH  640
 # define HEIGHT 480
 # define TEXTURE_WIDTH 100
@@ -37,13 +38,20 @@
 # define MOVE_SPEED 0.0125
 # define ROT_SPEED 0.015
 
-typedef struct s_hat	t_hat;
-
 typedef struct s_complex
 {
 	double	x;
 	double	y;
 }	t_complex;
+
+typedef struct s_player
+{
+	int			rotate;
+	t_complex	pos;
+	t_complex	dir;
+	t_complex	cam_plane;
+	t_complex	move;
+}	t_player;
 
 typedef struct s_img
 {
@@ -56,62 +64,7 @@ typedef struct s_img
 	int		endian;
 }	t_img;
 
-typedef struct s_texture
-{
-	void	*img_ptr;
-	char	*addr;
-	int		bpp;
-	int		width;
-	int		height;
-	int		line_len;
-	int		endian;
-}	t_texture;
 
-// yohai
-// mcombeau -> texture.c -> get_texture_index
-
-	// double		cam_x; // cameraX
-	// double		wall_x;
-	// double		wall_dist; // perpWallDist
-	// t_complex	cam_plane; // planeX
-	// t_complex	ray_dir; // rayDirX
-	// t_complex	cur_pos; // posX then mapX
-	// t_complex	side_dist; // sideDist
-	// t_complex	delta_dist; // deltaDist
-	// t_complex	step_dir; // stepX
-
-typedef struct s_raycast
-{
-	int			draw_start;
-	int			draw_end;
-	int			cur_pos_x;
-	int			cur_pos_y;
-	int			wall_height;
-	int			index_texture;
-	int			x_on_tex;
-	int			y_on_tex;
-	double		step;
-	double		cam_x;
-	double		side;
-	double		wall_dist;
-	double		wall_pos;
-	double		ray_height;
-	t_complex	xpm;
-	t_complex	cam_plane;
-	t_complex	ray_dir;
-	t_complex	side_dist;
-	t_complex	delta_dist;
-	t_complex	step_dir;
-}	t_raycast;
-
-typedef struct s_player
-{
-	int			rotate;
-	t_complex	pos;
-	t_complex	dir;
-	t_complex	cam_plane;
-	t_complex	move;
-}	t_player;
 
 typedef struct s_setup
 {
@@ -119,13 +72,11 @@ typedef struct s_setup
 	char		*so;
 	char		*ea;
 	char		*we;
-	int			*c;
-	int			*f;
-	int			ceiling;
-	int			floor;
+	int			c;
+	int			f;
 	int			orientation;
-	t_complex	pos;
-	t_complex	dir;
+	t_complex	pos; // fill with start pos
+	t_complex	dir; // fill with start dir (vector format)
 }	t_setup;
 
 typedef struct s_world // WOndeRlanD
@@ -145,7 +96,11 @@ typedef struct s_world // WOndeRlanD
 	t_raycast	*ray;
 }	t_world;
 
-int	clean_init(t_world *world);
-int	clean_exit(t_world *world);
+int		clean_init(t_world *world);
+int		clean_exit(t_world *world);
+
+// utils.c
+void	t_free(char **split);
+void	ft_clear(t_list *save);
 
 #endif
