8b32d9593fdf61c005b8bc3d42a9d20f93742f26
Tiffany GIBIER <tgibier@made-f0Br4s10.clusters.42paris.fr>
2023-10-19 16:35:40 +0200

    main for merge

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 #include "wonderland.h"
 #include "display.h"
+#include "error.h"
+#include "parsing.h"
 
 int	main(int argc, char **argv)
 {
 	t_world	world;
 
-	(void)argc;
-	(void)argv;
+	if (argc != 2)
+		return(ft_error_msg(ERR, &argc, ARG, USG), 1);
 	clean_init(&world);
+	if (parsing(&world, argv))
+		clean_exit(&world);
 	loop(&world);
 	clean_exit(&world);
 	return (1);
