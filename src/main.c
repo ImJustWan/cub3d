f247090a5e937e12f26ce904ede10de55e09bc45
Tiffany GIBIER <tgibier@made-f0Br4s10.clusters.42paris.fr>
2023-10-19 16:35:31 +0200

    main for merge

--- src/main.c
+++ src/main.c
@@ -7,17 +7,12 @@ int	main(int argc, char **argv)
 {
 	t_world	world;
 
-	(void)argc;
-	(void)argv;
 	if (argc != 2)
 		return(ft_error_msg(ERR, &argc, ARG, USG), 1);
 	clean_init(&world);
 	if (parsing(&world, argv))
 		clean_exit(&world);
-	// world.mlx_ptr = mlx_init();
-	// if (!world.mlx_ptr)
-	// 	return (1);
-	// window_init(&world);
+	loop(&world);
 	clean_exit(&world);
 	return (1);
 }
